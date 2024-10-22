0874bf65d7641f48f40019065f20e7a2734365e2
kike49 <enadalesc@hotmail.com>
2024-10-22 20:30:00 +0800

    lint

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -3,9 +3,6 @@ const path = require('path')
 const app = express()
 const serverless = require('serverless-http')
 
-const PORT = process.env.PORT || 5000
-
-
 // For API routes only
 app.get('/api/version', (req, res) => {
   res.send('1')
@@ -19,14 +16,14 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   // Development
   const PORT = process.env.PORT || 5000
-  
+
   // Serve static files only in development
   app.use(express.static('dist'))
-  
+
   // Handle React routing in development
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'dist', 'index.html'))
   })
-  
+
   app.listen(PORT, () => console.log(`Server running on port ${PORT}: http://localhost:${PORT}/`))
 }
